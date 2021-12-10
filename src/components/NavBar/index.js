294ba71c6db619d579c010f1456f74939c196cf4
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2021-12-09 19:46:31 -0600

    ejercicio boton con estado activo e inactivo

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+
+//estilos css
+import "./Navbar.css"
 
 export default function NavBar(props) {
+	const [active, setActive] = useState(0);
+	const itemOne = `item ${active === 1 ? "active" : "inactive"}`;
+	const itemTwo = `item ${active === 2 ? "active" : "inactive"}`;
+
 	return (
 		<div>
 			<ul>
-				<li>
-					<button onClick={props.onClick}>
+				<li className={itemOne} onClick={() => setActive(1)}>
 						FIRST ITEM
-					</button>
 				</li>
-				<li>
-				<button onClick={props.onClick}>
+				<li className={itemTwo} onClick={() => setActive(2)}>
 						SECOND ITEM
-					</button>
 				</li>
 			</ul>
 		</div>
