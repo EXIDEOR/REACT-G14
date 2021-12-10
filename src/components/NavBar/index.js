e3ef037f82f724e866ac6818b83523d2ba92d0cb
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2021-12-09 21:47:16 -0600

    ejercicio clase 4 navbar elements renderizados

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,8 +5,12 @@ import "./Navbar.css"
 
 export default function NavBar(props) {
 	const [active, setActive] = useState(0);
+	const checkActiveContent = (itemID) => (active === itemID ? true : false);
+
 	const itemOne = `item ${active === 1 ? "active" : "inactive"}`;
 	const itemTwo = `item ${active === 2 ? "active" : "inactive"}`;
+	const itemThree = `item ${active === 3 ? "active" : "inactive"}`;
+
 
 	return (
 		<div>
@@ -17,7 +21,14 @@ export default function NavBar(props) {
 				<li className={itemTwo} onClick={() => setActive(2)}>
 						SECOND ITEM
 				</li>
+				<li className={itemThree} onClick={() => setActive(3)}>
+						THIRD ITEM
+				</li>
 			</ul>
+			<div>{checkActiveContent(1) ? <h3> ◕3◕ </h3> : null}</div>
+			<div>{checkActiveContent(2) ? <h3> (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ </h3> : null}</div>
+			<div>{checkActiveContent(3) ? <h3> (☞ﾟ∀ﾟ)☞ </h3> : null}</div>
+			
 		</div>
 	);
 }
