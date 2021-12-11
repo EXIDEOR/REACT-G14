5fb2575bbc63cb02d8c2159f3ecf85e0dd5df1f8
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2021-12-10 20:52:34 -0600

    clase 4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {useEffect, useState} from "react"
 
-// Components
-import Counter from "./components/Counter";
-import NavBar from "./components/NavBar";
+const _users = [
+	{
+		id: 1,
+		firstName: "Selene",
+		lastName: "Chavez",
+	},
+	{
+		id: 2,
+		firstName: "Omar",
+		lastName: "Mijangos",
+	},
+	{
+		id: 3,
+		firstName: "Luis",
+		lastName: "Balán",
+	},
+	{
+		id: 4,
+		firstName: "Odon",
+		lastName: "Balán",
+	},
+];
+
 
 function App() {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		// await fetch()
+		setTimeout(() => {
+			const response = _users;
+			setUsers(response);
+		}, 3000);
+	}, []);
+
+	console.log(users);
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<Counter />
-				<NavBar />
+
+				{users.map((user) => {
+					const _firstName = user.firstName.toUpperCase();
+					return (
+						<div key={user.id}>
+							<h1>
+								{_firstName} {user.lastName}
+							</h1>
+						</div>
+					);
+				})}
+
+				{users.map((user) => (
+					<div key={user.id}>
+						<h1>
+							{user.firstName} {user.lastName}
+						</h1>
+					</div>
+				))}
 			</header>
 		</div>
 	);
