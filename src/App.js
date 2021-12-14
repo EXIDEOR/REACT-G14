b7f6c1a3c8510ec8b41e37cf32b254e6fbd4dde1
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2021-12-13 20:45:33 -0600

    clase 06

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 
 //components
 import UserName from "./components/UserName";
 
 const _users = [
-	{
-		id: 1,
-		firstName: "Auro",
-		lastName: "Escalera",
-	},
-	{
-		id: 2,
-		firstName: "Jairo",
-		lastName: "Roscano",
-	},
-	{
-		id: 3,
-		firstName: "Odon",
-		lastName: "Balán",
-	},
+  {
+    id: 1,
+    firstName: "Auro",
+    lastName: "Escalera",
+  },
+  {
+    id: 2,
+    firstName: "Jairo",
+    lastName: "Roscano",
+  },
+  {
+    id: 3,
+    firstName: "Odon",
+    lastName: "Balán",
+  },
 ];
 
-
 function App() {
-	const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-	useEffect(() => {
-		// await fetch()
-		setTimeout(() => {
-			const response = _users;
-			setUsers(response);
-		}, 1000);
-	}, []);
-	const usersUI = users.map(({id, firstName, lastName}) => (
-		<userName key={id} firstName={firstName} lastName={lastName} />
-	));
+  useEffect(() => {
+    // await fetch()
+    const getUsers = async () => {
+      const response = await fetch("users.json");
+      console.table(response);
+      const data = await response.json();
+      console.log(data);
+      setUsers(data);
+    };
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				{usersUI}
-			</header>
-			<UserName firstName={"Map"} lastName={"Out"}/>
-		</div>
-	);
+    getUsers();
+  }, []);
+  const usersUI = users.map(({ id, firstName, lastName }) => (
+    <userName key={id} firstName={firstName} lastName={lastName} />
+  ));
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {usersUI}
+      </header>
+      <UserName firstName={"Map"} lastName={"Out"} />
+    </div>
+  );
 }
 
 export default App;
