bdaef899cabe1ad12fff173fd04d67fdbacc4314
EXIDEOR <elfego.aguilar@alumnos.udg.mx>
2021-12-15 19:23:39 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
+  //debe reflejar un estado hacia el valor del input
+  const [input1, setInput1] = useState("");
+  const handleChange = (event) => {
+    console.log(event);
+  };
   return (
     <div className="container">
       <form className="form">
         <div className="flex">
           <label htmlFor="input1">Input:</label>
-          <input id="input1" value={"INICIAL"} />
+          <input id="input1" onChange={handleChange} value={input1} />
         </div>
       </form>
     </div>
